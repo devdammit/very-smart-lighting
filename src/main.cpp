cfdf68b2c7d0f7767af20725725de8840d7feefc
Anton Shchurov <ant.shchurov@gmail.com>
2023-07-09 19:31:10 +0700

    platform: revert framework to arduino

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
 #include <Arduino.h>
 
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  // Serial.println(TEST);
+  delay(1000);
+  Serial.println("Hello World!");
 }
